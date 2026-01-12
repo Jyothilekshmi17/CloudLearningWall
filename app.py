e471684fc33c9e3557131fedf8f5dff152bf460f
Jyothi <vrjyothilekshmi5@gmail.com>
2026-01-12 19:01:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,6 +28,7 @@ def index():
     data = supabase.table('learning_posts').select('*').order('id', desc=True).execute()
     return render_template('index.html', posts=data.data)
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
-if __name__ == '__main__':
-    app.run(debug=True)
